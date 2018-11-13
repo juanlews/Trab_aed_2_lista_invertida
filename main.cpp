c916ce3308be2da88c2d0f575e06e5ab1b61367f
juanlews <juanlews@hotmail.com>
2018-11-13 16:44:18 -0200

    Esqueleto

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+struct palavra{
+
+};
+
+void stopWords(){
+
+}
+
+void pesquisa(){
+
+}
+
+void criaIndices(char * palavra){
+
+}
+
+void lerArquivos(){
+
+}
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
