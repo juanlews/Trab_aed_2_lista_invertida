58c758deb4d9cecb7b97a82f13633ce7f7e6ce12
davigdc <36992852+davigdc@users.noreply.github.com>
2018-11-18 00:00:18 -0200

    funcao abrir arquivo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include<vector>
 using namespace std;
 
 struct palavra{
@@ -20,12 +23,23 @@ void criaIndices(char * palavra){
 
 void lerArquivos(){
 
+    FILE *aux= fopen("teste.txt", "r");
+    //char palavra[70];
+    //string palavra;
+    vector <string>palavra;
+    if(aux){
+        while(!feof(aux)){
+            fscanf(aux, "%[^\n]\n", &palavra);
+            printf("%s\n", palavra);
+        }
+    }
+
 }
 
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    lerArquivos();
+    cout<<endl<<"deu bom!";
     return 0;
 }
-
